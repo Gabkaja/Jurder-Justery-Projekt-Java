160bba936d66184796928f00b0e58f0089da66cd
Gabkaja <pater.gabriela86@gmail.com>
2026-05-24 16:37:11 +0200

    Create Motive.java
    world: add Motive model mapped to typy_motywow.json

---
+++ src/main/world/Motive.java
@@ -0,0 +1,11 @@
+package world;
+
+public class Motive {
+    private String id;
+    private String label;
+    private String description;
+
+    public String getId() { return id; }
+    public String getLabel() { return label; }
+    public String getDescription() { return description; }
+}
