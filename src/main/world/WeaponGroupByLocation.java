8464246fb821f3e616dcab17db5e01e7bc1a076b
Gabkaja <pater.gabriela86@gmail.com>
2026-05-24 16:34:58 +0200

    Create WeaponGroupByLocation.java
    world: add WeaponGroupByLocation model mapped to narzedzia_zbrodni.json

---
+++ src/main/world/WeaponGroupByLocation.java
@@ -0,0 +1,13 @@
+package world;
+
+import java.util.List;
+
+public class WeaponGroupByLocation {
+    private String locationId;
+    private String locationName;
+    private List<String> weapons;
+
+    public String getLocationId() { return locationId; }
+    public String getLocationName() { return locationName; }
+    public List<String> getWeapons() { return weapons; }
+}
