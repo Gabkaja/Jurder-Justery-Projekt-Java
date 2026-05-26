ec69b51d9a10e1f268312c8e341abe64b03d43c5
Natalia Pliś <natalia.plis@wp.pl>
2026-05-26 15:41:37 +0200

    Dodanie klas do obslugi wygladu terminala i kolorow

---
+++ src/main/ui/ScreenCleaner.java
@@ -0,0 +1,10 @@
+﻿package ui;
+
+public class ScreenCleaner {
+    // Czyści konsolę terminala i resetuje pozycję kursora.
+    public static void clear() {
+        // Sekwencja ANSI: \033[H (kursor na górę), \033[2J (wyczyść ekran)
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
---
+++ src/main/ui/TerminalUI.java
@@ -0,0 +1,29 @@
+﻿package ui;
+
+import java.util.List;
+
+public class TerminalUI {
+
+    /**
+     * Wyświetla pełny ekran lokacji: czyści stary widok, pokazuje gdzie gracz jest,
+     * wypisuje klimatyczny opis i listę akcji.
+     */
+    public static void showLocationScreen(String locationName, String description, List<String> options) {
+        // Czyszczenie ekranu przed nową sceną
+        ScreenCleaner.clear();
+
+        // Nagłówek z nazwą pokoju
+        MenuRenderer.renderHeader(locationName);
+
+        // Opis otoczenia
+        System.out.println("\n" + description);
+
+        // Menu wyboru dedykowane dla tej sytuacji
+        MenuRenderer.renderMenuOptions(options);
+    }
+
+    // Pomocnicza metoda do wyświetlania pilnych komunikatów (np. znalezienie dowodu).
+    public static void showNotification(String message) {
+        System.out.println("\n" + ColorManager.colorize("!!! " + message + " !!!", ColorManager.BOLD_RED));
+    }
+}
