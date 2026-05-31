1ef779c614b8f6554c92339c3af2fb36e2e65f66
Natalia Pliś <natalia.plis@wp.pl>
2026-05-31 23:44:31 +0200

    Poprawa dialogów i dowodów

--- src/main/java/world/EventLog.java
+++ src/main/java/world/EventLog.java
@@ -1,38 +1,33 @@
 package world;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-/**
- * Tymczasowa atrapa klasy EventLog, żeby gra się uruchomiła.
- */
 public class EventLog {
 
-    // Lista przechowująca wpisy (na razie będzie pusta)
     private final List<String> entries = new ArrayList<>();
+    private final Set<String> uniqueClues = new HashSet<>();
 
     public EventLog() {
-        // Konstruktor domyślny, nic nie musi robić
     }
 
-    // Metoda, której szuka interfejs InvestigationSystem
     public List<String> getEntries() {
         return entries;
     }
 
-    // Metoda do dodawania nowych zdarzeń (przyda się na przyszłość)
     public void addEntry(String entry) {
         this.entries.add(entry);
     }
 
-    private final List<String> clues = new ArrayList<>();
-
     public List<String> getClues() {
-        return clues;
+        return new ArrayList<>(uniqueClues);
     }
 
     public void addClue(String clue) {
-        this.clues.add(clue);
+        if (uniqueClues.add(clue)) {
+            this.entries.add("Znaleziono nowy dowód: " + clue);
+        }
     }
-
 }
